51a7f7fc517deecfa159bba8cad15b27e0943aa6
Razziell <razziell@ya.ru>
2024-06-14 20:56:56 +0300

    sm6150-common: build lineage health HAL

--- sm6150.mk
+++ sm6150.mk
@@ -200,6 +200,10 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # HIDL
 PRODUCT_PACKAGES += \
     libhidltransport.vendor \
